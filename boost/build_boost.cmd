69a152ed0072d8770c4327d07f58994dfbb3cd86
Surpreem <surpreem+sn@gmail.com>
2017-03-28 16:28:11 +0900

    boost 라이브러리를 static, shared 모든 조합으로 빌드하도록 변경

--- boost/build_boost.cmd
+++ boost/build_boost.cmd
@@ -6,21 +6,15 @@ SET BUILD_DIR=%TEMP%
 
 
 call bootstrap.bat
-b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x64 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x64.jam toolset=msvc-14.0 address-model=64 variant=debug,release link=shared threading=multi runtime-link=shared stage > build_log_x64.txt
-
-FIND /I "error" build_log_x64.txt > build_errors_x64.txt
-FIND /I "warning" build_log_x64.txt > build_warnings_x64.txt
+b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x64 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x64.jam toolset=msvc-14.0 --build-type=complete stage
 
 call bootstrap.bat
-b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x86 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x86.jam toolset=msvc-14.0 variant=debug,release link=shared threading=multi runtime-link=shared stage > build_log_x86.txt
+b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x86 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x86.jam toolset=msvc-14.0 --build-type=complete stage
 
-FIND /I "error" build_log_x86.txt > build_errors_x86.txt
-FIND /I "warning" build_log_x86.txt > build_warnings_x86.txt
-
-move /y %STAGE_DIR%x64\lib\* %STAGE_DIR%x64
-move /y %STAGE_DIR%x86\lib\* %STAGE_DIR%x86
-rd /s/q %STAGE_DIR%x64\lib
-rd /s/q %STAGE_DIR%x86\lib
+robocopy /E /COPYALL "%STAGE_DIR%x64\lib" "%STAGE_DIR%x64"
+robocopy /E /COPYALL "%STAGE_DIR%x86\lib" "%STAGE_DIR%x86"
+rmdir /S /Q "%STAGE_DIR%x64\lib"
+rmdir /S /Q "%STAGE_DIR%x86\lib"
 del *.exe
 
 ENDLOCAL
