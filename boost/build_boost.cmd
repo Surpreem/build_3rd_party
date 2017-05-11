9c9147eff94e92550338fc5c1491fecde480467e
Surpreem <surpreem+sn@gmail.com>
2017-05-12 08:26:54 +0900

    Boost 빌드 스크립트 옵션 수정
    address-model을 지정하지 않으면 Boost.python을 빌드할 때 호스트 플랫폼을 잘못
    인식해 대상 플랫폼과 일치하지 않는다는 오류를 발생하며 파이썬 라이브러리를 링크
    하지 못할 수 있다. x64용을 빌드할 때는 꼭 지정해야 하며 x86용을 빌드할 때는 지정
    하지 않아도 되나 스크립트를 일치시키기 위해 추가한다.

--- boost/build_boost.cmd
+++ boost/build_boost.cmd
@@ -6,10 +6,10 @@ SET BUILD_DIR=%TEMP%
 
 
 call bootstrap.bat
-b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x64 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x64.jam toolset=msvc-14.0 --build-type=complete stage
+b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x64 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x64.jam toolset=msvc-14.0 address-model=64 --build-type=complete stage
 
 call bootstrap.bat
-b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x86 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x86.jam toolset=msvc-14.0 --build-type=complete stage
+b2 -j4 -a --reconfigure --stagedir=%STAGE_DIR%x86 --build-dir=%BUILD_DIR% --without-mpi --user-config=user-config_x86.jam toolset=msvc-14.0 address-model=32 --build-type=complete stage
 
 robocopy /E /COPYALL "%STAGE_DIR%x64\lib" "%STAGE_DIR%x64"
 robocopy /E /COPYALL "%STAGE_DIR%x86\lib" "%STAGE_DIR%x86"
